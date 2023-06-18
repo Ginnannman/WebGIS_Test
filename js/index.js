3c0b6d7f0b136011ed26637f3785c688bf57ee2e
Ginnannman <52596362+Ginnannman@users.noreply.github.com>
2023-06-18 15:13:41 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -97,6 +97,9 @@ class MyLineSymbolizer{
         var colorInt= parseInt(feature.props["hougaku"]*93206.75);
         var color16= '0x' + colorInt.toString(16);
         context.strokeStyle = color16;
+        context.fillStyle = colors16;
+        context.beginPath();
+        context.fill();
         context.stroke();
     }
 }
