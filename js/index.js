4fff3db8e590bd3a8acb36f77cc53188bf646a5c
Ginnannman <52596362+Ginnannman@users.noreply.github.com>
2023-06-18 17:04:03 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -98,10 +98,18 @@ group.on('add', function(){
   //MSAIRoadDetections
 class MyLineSymbolizer{
     draw(context,geom,z,feature){
+        
         var colorInt= parseInt(feature.props["hougaku"]*93206.75);
         var color16= '#' + colorInt.toString(16);
         context.strokeStyle = color16;
         context.beginPath();
+        for (var poly of geom) {
+            for (var p = 0; p < poly.length-1; p++) {
+                let pt = poly[p];
+                if (p == 0) context.moveTo(pt.x,pt.y);
+                else context.lineTo(pt.x,pt.y);
+            }
+        }
         context.stroke();
     }
 }
